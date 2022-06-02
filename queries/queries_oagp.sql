28c733af16d0ac5d4746536af485da7d3f39c5c0
billstam12 <billstam12@gmail.com>
2022-06-02 12:26:18 +0300

    changed gitignore

--- queries/queries_oagp.sql
+++ queries/queries_oagp.sql
@@ -1,68 +1,24 @@
--- noinspection SqlDialectInspectionForFile
+-- 5, 10, 15, 20, 25, 30, 35, 40, 45, 50
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 20) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 10) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 6.6) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 5) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 4) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 3.3) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 2.85) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 2.5) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 2.22) < 1
+SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 2) < 1
 
--- SELECT 1 FROM oag.papers200k
---
--- --Q1
--- --Q2
--- --Q3
--- --Q4
--- --Q5
---
---
--- SELECT DEDUP * FROM oag.papers200k WHERE year > 2012
--- SELECT DEDUP * FROM oag.papers200k WHERE year > 2008
--- SELECT DEDUP * FROM oag.papers200k WHERE year > 2003
--- SELECT DEDUP * FROM oag.papers200k WHERE year > 1995
--- SELECT DEDUP * FROM oag.papers200k WHERE year > 1990
---
--- SELECT DEDUP * FROM oag.papers500k WHERE year > 2012
--- SELECT DEDUP * FROM oag.papers500k WHERE year > 2008
--- SELECT DEDUP * FROM oag.papers500k WHERE year > 2003
--- SELECT DEDUP * FROM oag.papers500k WHERE year > 1995
--- SELECT DEDUP * FROM oag.papers500k WHERE year > 1990
---
--- SELECT DEDUP * FROM oag.papers1m WHERE year > 2012
--- SELECT DEDUP * FROM oag.papers1m WHERE year > 2008
--- SELECT DEDUP * FROM oag.papers1m WHERE year > 2003
--- SELECT DEDUP * FROM oag.papers1m WHERE year > 1995
--- SELECT DEDUP * FROM oag.papers1m WHERE year > 1990
---
--- SELECT DEDUP * FROM oag.papers1m500k WHERE year > 2012
--- SELECT DEDUP * FROM oag.papers1m500k WHERE year > 2008
--- SELECT DEDUP * FROM oag.papers1m500k WHERE year > 2003
--- SELECT DEDUP * FROM oag.papers1m500k WHERE year > 1995
--- SELECT DEDUP * FROM oag.papers1m500k WHERE year > 1990
---
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 2012
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 2008
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 2003
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 1995
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 1990
---
--- SELECT DEDUP * FROM oag.papers5m WHERE year > 2012
--- SELECT DEDUP * FROM oag.papers5m WHERE year > 2008
--- SELECT DEDUP * FROM oag.papers5m WHERE year > 2003
--- SELECT DEDUP * FROM oag.papers5m WHERE year > 1995
--- SELECT DEDUP * FROM oag.papers5m WHERE year > 1990
---
--- --Q9 fixed selectivity, increasing dataset size
---
--- SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 4) < 1
--- SELECT DEDUP * FROM oag.papers200k WHERE MOD(id, 10) < 1
--- SELECT DEDUP * FROM oag.papers200k  WHERE MOD(index, 20) < 1
--- SELECT DEDUP * FROM oag.papers200k  WHERE MOD(index, 15) < 1
--- SELECT DEDUP * FROM oag.papers200k  WHERE MOD(index, 4) < 1
--- SELECT DEDUP * FROM oag.papers1m WHERE MOD(index, 50) < 1
-SELECT DEDUP * FROM oag.papers1m WHERE MOD(index, 3) < 1
--- SELECT DEDUP * FROM oag.papers1m500k WHERE MOD(id, 30) < 1
--- SELECT DEDUP * FROM oag.papers2m WHERE MOD(id, 40) < 1
---
---
--- --Q10-Q13 Effects of LI, run this with links enabled,
---
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 2008 AND year < 2012
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 2004 AND year < 2012
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 2000 AND year < 2012
--- SELECT DEDUP * FROM oag.papers2m WHERE year > 1994 AND year < 2012
---
---
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 20) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 10) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 6.6) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 5) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 4) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 3.3) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 2.85) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 2.5) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 2.22) < 1
+SELECT DEDUP * FROM oag.papers1m WHERE MOD(id, 2) < 1
+
+-- Overlapping 5, 15, 25, 35, 45
